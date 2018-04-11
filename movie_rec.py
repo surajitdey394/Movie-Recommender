5f8d23e3d2e9d8fc0a38c3feecc7797bd8c30848
surajitdey394 <surajitdey394.sd@gmail.com>
2018-04-11 17:00:28 +0530

    Update movie_rec.py

--- movie_rec.py
+++ movie_rec.py
@@ -20,34 +20,69 @@ from surprise import Reader, Dataset, SVD, evaluate
 import warnings; warnings.simplefilter('ignore')
 
 md = pd. read_csv('../input/movies_metadata.csv')
-md.head()
-
-links_small = pd.read_csv('../input/links_small.csv')
-links_small = links_small[links_small['tmdbId'].notnull()]['tmdbId'].astype('int')
-md = md.drop([19730, 29503, 35587])
+credits = pd.read_csv('../input/credits.csv')
+keywords = pd.read_csv('../input/keywords.csv')
+keywords['id'] = keywords['id'].astype('int')
+credits['id'] = credits['id'].astype('int')
 md['id'] = md['id'].astype('int')
-
-#description based
-smd = md[md['id'].isin(links_small)]
-smd.shape
-smd['tagline'] = smd['tagline'].fillna('')
-smd['description'] = smd['overview'] + smd['tagline']
-smd['description'] = smd['description'].fillna('')
-tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
-tfidf_matrix = tf.fit_transform(smd['description'])
-tfidf_matrix.shape
-#cosine similarity
-cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-cosine_sim[0]
+md = md.merge(credits, on='id')
+md = md.merge(keywords, on='id')
+smd = md[md['id'].isin(links_small)]smd['cast'] = smd['cast'].apply(literal_eval)
+smd['crew'] = smd['crew'].apply(literal_eval)
+smd['keywords'] = smd['keywords'].apply(literal_eval)
+smd['cast_size'] = smd['cast'].apply(lambda x: len(x))
+smd['crew_size'] = smd['crew'].apply(lambda x: len(x))
+def get_director(x):
+    for i in x:
+        if i['job'] == 'Director':
+            return i['name']
+    return np.nan
+smd['director'] = smd['crew'].apply(get_director)
+smd['cast'] = smd['cast'].apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+smd['cast'] = smd['cast'].apply(lambda x: x[:3] if len(x) >=3 else x)
+smd['keywords'] = smd['keywords'].apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+smd['cast'] = smd['cast'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+smd['director'] = smd['director'].astype('str').apply(lambda x: str.lower(x.replace(" ", "")))
+smd['director'] = smd['director'].apply(lambda x: [x,x, x])
+s = smd.apply(lambda x: pd.Series(x['keywords']),axis=1).stack().reset_index(level=1, drop=True)
+s.name = 'keyword'
+s = s[s > 1]
+stemmer = SnowballStemmer('english')
+stemmer.stem('dogs')
+def filter_keywords(x):
+    words = []
+    for i in x:
+        if i in s:
+            words.append(i)
+    return words
+smd['keywords'] = smd['keywords'].apply(filter_keywords)
+smd['keywords'] = smd['keywords'].apply(lambda x: [stemmer.stem(i) for i in x])
+smd['keywords'] = smd['keywords'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+smd['soup'] = smd['keywords'] + smd['cast'] + smd['director'] + smd['genres']
+smd['soup'] = smd['soup'].apply(lambda x: ' '.join(x))
+smd['soup'].head()
+count = CountVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
+count_matrix = count.fit_transform(smd['soup'])
 smd = smd.reset_index()
 titles = smd['title']
 indices = pd.Series(smd.index, index=smd['title'])
-def get_recommendations(title):
+def recommendations(title):
     idx = indices[title]
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:31]
+    sim_scores = sim_scores[1:26]
     movie_indices = [i[0] for i in sim_scores]
-    return titles.iloc[movie_indices]
+    
+    movies = smd.iloc[movie_indices][['title', 'vote_count', 'vote_average', 'year']]
+    vote_counts = movies[movies['vote_count'].notnull()]['vote_count'].astype('int')
+    vote_averages = movies[movies['vote_average'].notnull()]['vote_average'].astype('int')
+    C = vote_averages.mean()
+    m = vote_counts.quantile(0.60)
+    qualified = movies[(movies['vote_count'] >= m) & (movies['vote_count'].notnull()) & (movies['vote_average'].notnull())]
+    qualified['vote_count'] = qualified['vote_count'].astype('int')
+    qualified['vote_average'] = qualified['vote_average'].astype('int')
+    qualified['wr'] = qualified.apply(weighted_rating, axis=1)
+    qualified = qualified.sort_values('wr', ascending=False).head(10)
+    return qualified
 
-get_recommendation(input())
+recommendations(input())
